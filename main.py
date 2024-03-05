9607929fec2778403645c7eead52950cc6fbddd7
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-03-05 12:55:13 +0530

    Wrote the logic for ensuring that only new files are considered for appending in the GDoc

--- main.py
+++ main.py
@@ -15,28 +15,43 @@ class QuestionRev:
         self.filesList = os.listdir(self.directories[0])
         #print(self.filesList)
     
-    def filterFiles(self):
+    def filterGoodFiles(self):
         # We will filter all the files that contain good in there names
         self.filesList = [x for x in self.filesList if x[-10:-6].lower() == 'good']
         #print(100,100)
         #print(self.filesList)
-        
+    
+    def filterNewFiles(self):
+        lastUsedQuestionNumber = ""
+        with open("lastUsedFile.txt","r") as lastUsedFile:
+            lastUsedQuestionNumber = lastUsedFile.read()
+        print(lastUsedQuestionNumber,len(lastUsedQuestionNumber),type(lastUsedQuestionNumber)," <- Last used question Number")
+        if lastUsedQuestionNumber != "":
+            ctr = 0
+            flag = False
+            while ctr < len(self.filesList):
+                if lastUsedQuestionNumber == self.filesList[ctr][0:len(lastUsedQuestionNumber)] and not flag:
+                    flag = not flag
+                elif flag and self.filesList[ctr][0:len(lastUsedQuestionNumber)].isnumeric():
+                    break
+                ctr += 1
+            self.filesList = self.filesList[ctr:]   # We will handle only new files 
+        else:   # We don't want any changes
+            self.filesList = self.filesList
+        print("LIST OF FILES")
+        print(self.filesList)
+                
     def openTheFiles(self):
         for i in range(len(self.filesList)):
             currFileName = self.filesList[i]
-            self.filesContent.append(currFileName)
+            self.filesContent.append("\n")
+            self.filesContent.append(currFileName + "\n")
         # We will open the filtered files and will place their content
             with open(self.directories[0] + r'\\' + currFileName,"r") as notebookFile:
                 notebookContent = nbformat.read(notebookFile,as_version=4)
             for cell in notebookContent.cells:
                 if cell.cell_type == "code":
-                    self.filesContent.append(cell.source)        # the type of cell.content = str
-    '''
-    def addExtraSpaces(self):
-        temp = self.filesContent
-        for i in range(len(temp)):
-            if temp[0:2].isnumeric():
-     '''           
+                    self.filesContent.append(cell.source + "\n")        # the type of cell.content = str
     
     def writeTheContentToFile(self):
         with open("output.txt","w") as outputFile:
@@ -50,11 +65,10 @@ class QuestionRev:
     
     def execute(self):
         self.createList()
-        self.filterFiles()
+        self.filterGoodFiles()
+        self.filterNewFiles()
+        self.openTheFiles()
+        self.writeTheContentToFile()
 
 q = QuestionRev()
-q.createList()
-q.filterFiles()
-q.openTheFiles()
-#print(q.filesContent)
-q.writeTheContentToFile()
+q.execute()
