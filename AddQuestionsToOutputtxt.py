450c1b63fe6c37bb41894d866994a52cb9cead50
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-03-06 00:11:26 +0530

    Changed the layout with main.py being the only file to be exectured to fulfil the tasks

---
+++ AddQuestionsToOutputtxt.py
@@ -0,0 +1,89 @@
+# Open file in the given directory and then check if the file has good in its name in last 4 place
+
+import os
+#import codecs
+import nbformat
+
+class QuestionRev:
+    def __init__(self):
+        self.directories = [r"G:\Python Exercises\Greg Hoff"]
+        self.filesList =[]
+        self.filesContent = []
+    
+    def createList(self):
+        # We will create the list of all the files in the given directories
+        self.filesList = os.listdir(self.directories[0])
+        #print(self.filesList)
+    
+    def filterGoodFiles(self):
+        # We will filter all the files that contain good in there names
+        self.filesList = [x for x in self.filesList if x[-10:-6].lower() == 'good']
+        #print(100,100)
+        #print(self.filesList)
+    
+    def filterNewFiles(self):
+        lastUsedQuestionNumber = ""
+        with open("lastUsedFile.txt","r") as lastUsedFile:
+            lastUsedQuestionNumber = lastUsedFile.read()
+        #print(lastUsedQuestionNumber,len(lastUsedQuestionNumber),type(lastUsedQuestionNumber)," <- Last used question Number")
+        if lastUsedQuestionNumber != "":
+            ctr = 0
+            flag = False
+            while ctr < len(self.filesList):
+                if lastUsedQuestionNumber == self.filesList[ctr][0:len(lastUsedQuestionNumber)] and not flag:
+                    flag = not flag
+                elif flag and self.filesList[ctr][0:len(lastUsedQuestionNumber)].isnumeric():
+                    break
+                ctr += 1
+            self.filesList = self.filesList[ctr:]   # We will handle only new files 
+        else:   # We don't want any changes
+            self.filesList = self.filesList
+        #print("LIST OF FILES")
+        #print(self.filesList)
+                
+    def openTheFiles(self):
+        dashedLine = '\n' + "_"*74 + '\n'
+        for i in range(len(self.filesList)):
+            currFileName = self.filesList[i]
+            self.filesContent.append(dashedLine)
+            #self.filesContent.append("\n")
+            self.filesContent.append(currFileName + "\n")
+        # We will open the filtered files and will place their content
+            with open(self.directories[0] + r'\\' + currFileName,"r") as notebookFile:
+                notebookContent = nbformat.read(notebookFile,as_version=4)
+            for cell in notebookContent.cells:
+                if cell.cell_type == "code":
+                    self.filesContent.append(cell.source + "\n")        # the type of cell.source = str
+    
+    def writeTheContentToFile(self):
+        with open("output.txt","w") as outputFile:
+            outputFile.write("")
+        for i in range(len(self.filesContent)):
+            with open("output.txt","a+") as outputFile:
+                outputFile.write(self.filesContent[i])
+        fileSize = os.path.getsize("output.txt")
+        print(f"Output.txt of file size {fileSize} has been written")
+    
+    def updateTheLastQuestionNumber(self):
+        print(self.filesList)
+        if len(self.filesList) == 0:
+            return
+        lastFileName = self.filesList[-1]
+        lastFileNumber = ""
+        for i in lastFileName:
+            if i.isnumeric():
+                lastFileNumber += i
+            else:
+                break
+        with open("lastUsedFile.txt","w") as outputFile:
+            outputFile.write(lastFileNumber)
+        
+            
+    
+    def execute(self):
+        self.createList()
+        self.filterGoodFiles()
+        self.filterNewFiles()
+        self.openTheFiles()
+        self.writeTheContentToFile()
+        self.updateTheLastQuestionNumber()
---
+++ UploadDataToDrive.py
@@ -0,0 +1,80 @@
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseUpload, MediaIoBaseDownload
+from google.auth.transport.requests import Request
+import io
+import pickle
+import os
+import time
+
+scopes = ['https://www.googleapis.com/auth/drive']
+credsFile = "credentials.json"
+tokenPickleFile = "token.pickle"
+
+class uploadData:
+    def __init__(self):
+        self.newCodes, self.fileID = None, None
+        with open('output.txt','r') as file:
+            self.newCodes = file.read()
+        with open('fileName.txt','r') as file:
+            self.fileID = file.read()
+        
+    def getAuthenticatedService(self):
+        creds = None
+        if os.path.exists(tokenPickleFile):
+            with open(tokenPickleFile,"rb") as tokenFile:
+                creds = pickle.load(tokenFile)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(credsFile,scopes)
+                creds = flow.run_local_server(port=0)
+            with open(tokenPickleFile,"wb") as token:
+                pickle.dump(creds,token)
+        return build("drive","v3",credentials=creds)
+    def updateFile(self,fileID,newContent):
+        service = self.getAuthenticatedService()
+        mediaBody = MediaIoBaseUpload(io.BytesIO(newContent.encode()), mimetype='text/plain', resumable=True)
+        updatedFile = service.files().update(fileId=fileID, media_body=mediaBody).execute()
+        return updatedFile
+
+    def appendToFile(self,fileID,additionalContent):
+        service = self.getAuthenticatedService()
+        fileMetadata = service.files().get(fileId=fileID,fields='mimeType').execute()
+        mimeType = fileMetadata.get('mimeType','')
+        if mimeType.startswith('application/vnd.google-apps'):
+            request = service.files().export_media(fileId=fileID,mimeType="text/plain")
+            exportedContent = request.execute()
+            exportedContentString = exportedContent.decode('utf-8')
+            #print(exportedContentString)
+            newContent = exportedContentString + '\n' + additionalContent
+            newContentIO = io.BytesIO(newContent.encode())
+            mediaBody = MediaIoBaseUpload(newContentIO,mimetype ='text/plain',resumable=True)
+            updatedFile = service.files().update(fileId=fileID,media_body=mediaBody).execute()
+        return updatedFile
+    
+    def execute(self):
+        self.appendToFile(self.fileID,additionalContent=self.newCodes)
+
+obj = uploadData()
+obj.execute()
+
+'''       
+startTime = time.time()  
+with open('fileName.txt','r') as file:
+    fileID = file.read()
+#print(fileID,type(fileID))
+#fileID = "1iptEZfA0p6ZZThUrPaOS4O44f7umeWwdvUG4Jjc_kSw"
+
+newCodes = None
+with open('output.txt','r') as file:
+    newCodes = file.read()
+#print(newCodes, type(newCodes))
+up = uploadData()
+print(up.appendToFile(fileID,additionalContent = newCodes))
+print(f"Time taken is {time.time()-startTime}")
+#updatedFile = updateFile(fileID,newContent)
+#print('File updated : ',updatedFile)
+'''
